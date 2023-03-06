27fee8c5d535109b4f5fdb23dd240a984b1c8b6f
Elisabeth Messmer <elisabethsophiem@web.de>
2023-03-06 15:16:34 +0100

    added footer in shared

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_092645) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_140338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.date "date"
+    t.time "time"
+    t.integer "capacity"
+    t.string "sport"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

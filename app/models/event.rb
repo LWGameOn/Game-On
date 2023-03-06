06a16e63cba92fed299e2de8653318a639340796
Yesim Dundar <kuranyesim@gmail.com>
2023-03-06 18:57:55 +0100

    Add user, event and play relations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  # shows whoever created the event
+  belongs_to :user
+
+  # shows whoever is participating in the event
+  has_many :users, through: :plays
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Since we can't have two different "has_many :events" definitions, we should
+  # distinguish between participation and event ownership
+  # shows whoever has created the events
+  has_many :event_invites, foreign_key: 'user_id', class_name: 'Event'
+
+  # shows whoever is participating in the event
+  has_many :events, through: :plays
+
+  has_many :plays
 end

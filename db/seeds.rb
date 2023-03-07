c8e60f8147bc67676b9340306eb16d1ba29af745
Yesim Dundar <kuranyesim@gmail.com>
2023-03-07 17:40:44 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,22 @@ User.destroy_all
 end
 
 
-
+i = 0
   20.times do
     user = User.all.sample
-    sport = ["football", "basketball", "volleyball", "tennis", "baseball"].sample
+    sport = ["football", "basketball", "volleyball", "tennis", "baseball"]
+    sport_type = sport.sample
     level = ["advance", "beginner", "intermediate"].sample
    Event.create(
-     name: sport,
-     description: "I'm looking for #{sport} enthusiasts. If you are one of them. Please feel free to join our team. It will be #{level} friendly.",
+     name: "#{sport_type.capitalize} Time",
+     description: "I'm looking for #{sport[i]} enthusiasts. If you are one of them. Please feel free to join our team. It will be #{level} friendly.",
      location: Faker::Address.full_address,
      date: Faker::Date.forward(days: 365),
      time: Faker::Time.forward(days: 23, format: :long).split(" ")[3],
      capacity: rand(2..20),
-     sport: Faker::Sports,
+     sport: sport_type,
      user_id: user.id
    )
+   i += 1
   end
+  puts "Finished"

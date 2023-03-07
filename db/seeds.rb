966a3e78610682fb803717eb183b8b25ab176865
Yesim Dundar <kuranyesim@gmail.com>
2023-03-07 15:04:19 +0100

    create events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Event.destroy_all
 User.destroy_all
 
 
 #
-40.times do
-  User.new(email: Faker::Internet.email, password: "gameon1312", username: Faker::Internet.username).save
+20.times do
+  User.new(
+    email: Faker::Internet.email,
+    password: "gameon1312",
+    username: Faker::Internet.username
+    ).save
 end
 
+
+
+  20.times do
+    user = User.all.sample
+    sport = Faker::Sports
+    level = ["advance", "begginer","intermediate"].sample
+   Event.new(
+     name:sport,
+     description: "I'm looking for #{sport} enthusiasts. If you are one of them. Please feel free to join our team. It will be #{level} friendly.",
+     location: Faker::Address.full_address,
+     date: Faker::Date.forward(days: 365),
+     time: Faker::Time.forward(days: 23, format: :long).split(" ")[3],
+     capacity: rand(2..20),
+     sport: Faker::Sports
+   ).save
+  end

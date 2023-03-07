646cf23b190785eb9ef7787b63b0df1f953dfdaa
Yesim Dundar <kuranyesim@gmail.com>
2023-03-07 15:43:48 +0100

    generate events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,31 @@
+require 'faker'
 Event.destroy_all
 User.destroy_all
 
 
 #
-<<<<<<< HEAD
 20.times do
-  User.new(
+  User.create(
     email: Faker::Internet.email,
     password: "gameon1312",
     username: Faker::Internet.username
-    ).save
+    )
 end
 
 
 
   20.times do
     user = User.all.sample
-    sport = Faker::Sports
-    level = ["advance", "begginer","intermediate"].sample
-   Event.new(
-     name:sport,
+    sport = ["football", "basketball", "volleyball", "tennis", "baseball"].sample
+    level = ["advance", "beginner", "intermediate"].sample
+   Event.create(
+     name: sport,
      description: "I'm looking for #{sport} enthusiasts. If you are one of them. Please feel free to join our team. It will be #{level} friendly.",
      location: Faker::Address.full_address,
      date: Faker::Date.forward(days: 365),
      time: Faker::Time.forward(days: 23, format: :long).split(" ")[3],
      capacity: rand(2..20),
-     sport: Faker::Sports
-   ).save
+     sport: Faker::Sports,
+     user_id: user.id
+   )
   end
-=======
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
->>>>>>> master

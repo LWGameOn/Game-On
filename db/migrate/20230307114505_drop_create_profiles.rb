48c91984499e5047ddc9c6fc939ecfeb0c28f1a1
Yesim Dundar <kuranyesim@gmail.com>
2023-03-07 12:48:21 +0100

    drop profiles table

---
+++ db/migrate/20230307114505_drop_create_profiles.rb
@@ -0,0 +1,6 @@
+class DropCreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :profiles
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_145323) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_114505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,34 +75,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_145323) do
     t.index ["user_id"], name: "index_plays_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "name"
-    t.text "bio"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_messages_on_event_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "plays", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_plays_on_event_id"
-    t.index ["user_id"], name: "index_plays_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -124,6 +94,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_145323) do
   add_foreign_key "messages", "users"
   add_foreign_key "plays", "events"
   add_foreign_key "plays", "users"
-  add_foreign_key "profiles", "users"
-
 end

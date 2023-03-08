f97d922c155415db8577192322ae4ad50f3d7d67
Elisabeth Messmer <elisabethsophiem@web.de>
2023-03-08 09:15:07 +0100

    geocode and mapbox setup

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,9 @@ class Event < ApplicationRecord
 
   has_many :messages
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   validates :name, :description, :location, :date, :time, presence: true
   validates :description, length: { minimum: 30 }
 end

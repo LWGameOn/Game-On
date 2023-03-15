69f6c978edf7de0c6e2f5c6ae084af2962791619
sbcodes <stefan.bachmann123@gmail.com>
2023-03-15 11:31:31 +0100

    feat(users_controller.rb): add filter functionality to show method feat(users_controller.rb): filter events and plays based on filter parameter feat(users_controller.rb): show past events and plays if filter parameter is 'past' feat(users_controller.rb): show upcoming events and plays if filter parameter is not 'past' feat(users_controller.rb): add filter links to show view feat(users_controller.rb): add 'Upcoming' filter link to show view feat(users_controller.rb): add 'Past' filter link to show view feat(users_controller.rb): show only events posted by the user feat(users_controller.rb): show only plays joined by the user feat(users_controller.rb): exclude events posted by the user from plays joined by the user feat(users_controller.rb): remove redundant Event.where and Play.where calls in show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
 
   def show
     @report = Report.new
+    if params[:filter] == 'past'
+      @events = Event.where(user: @user).select { |e| e.date.before?(DateTime.now) }
+      @plays = Play.where(user: @user).select { |p| p.event.date.before?(DateTime.now) }
+    else
+      @events = Event.where(user: @user).select { |e| e.date.after?(DateTime.now) }
+      @plays = Play.where(user: @user).select { |p| p.event.date.after?(DateTime.now) }
+    end
   end
 
   def edit
